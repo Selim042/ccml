887865c77f17298586ba02042d1e7f72a6053ec3
Emily <042Selim@gmail.com>
2025-03-06 15:34:56 -0500

    Allow logger to print to monitor

--- logger.lua
+++ logger.lua
@@ -5,11 +5,27 @@ api.open = function(filePath)
   local debugEnabled = false
   local isClosed = false
   local timeOffset = 0
+  local monitor = nil
 
-  local function writeLine(msg)
+  local function fileWrite(msg)
+    file.write(msg)
+    file.flush()
+    if (monitor ~= nil) then
+      monitor.write(msg)
+    end
+  end
+
+  local function fileWriteLine(msg)
     file.writeLine(msg)
     file.flush()
+    if (monitor ~= nil) then
+      monitor.write(msg..'\n')
+      monitor.scroll(1)
+      local monW,monH = monitor.getSize()
+      monitor.setCursorPos(1,monH)
+    end
   end
+
   local function writeTime()
     local time = os.time('utc') + timeOffset
     if (time < 0) then
@@ -19,39 +35,39 @@ api.open = function(filePath)
     if (hours < 10) then
       hours = '0'..hours
     end
-    file.write('[')
-    file.write(hours)
-    file.write(':')
+    fileWrite('[')
+    fileWrite(hours)
+    fileWrite(':')
     local mins = math.floor(60*(time - math.floor(time)))
     if (mins < 10) then
       mins = '0'..mins
     end
-    file.write(mins)
-    file.write(']')
+    fileWrite(mins)
+    fileWrite(']')
   end
 
   local ret = {}
   ret.info = function(msg)
     if (not isClosed) then
       writeTime()
-      file.write(' [INFO] ')
-      writeLine(msg)
+      fileWrite(' [INFO] ')
+      fileWriteLine(msg)
     end
   end
 
   ret.error = function(msg)
     if (not isClosed) then
       writeTime()
-      file.write(' [ERROR] ')
-      writeLine(msg)
+      fileWrite(' [ERROR] ')
+      fileWriteLine(msg)
     end
   end
 
   ret.debug = function(msg)
     if (not closed and debugEnabled) then
       writeTime()
-      file.write(' [DEBUG] ')
-      writeLine(msg)
+      fileWrite(' [DEBUG] ')
+      fileWriteLine(msg)
     end
   end
 
@@ -61,6 +77,12 @@ api.open = function(filePath)
 
   ret.setTimeOffset = function(offset)
     timeOffset = offset
+  end
+
+  ret.setMonitor = function(mon)
+    monitor = mon
+    local monW,monH = monitor.getSize()
+    monitor.setCursorPos(1,monH)
   end
 
   ret.close = function()
