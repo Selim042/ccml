066e3441e3fc9a1aaeff2ec118835af3a4b43ff9
Emily <042Selim@gmail.com>
2025-03-30 13:45:53 -0400

    Start documenting tags

--- tags/align.lua
+++ tags/align.lua
@@ -1,6 +1,18 @@
+local validAlignment = {
+    'center',
+    'right'
+}
+
+--- Aligns contained elements either to center or right of containing element.
+--- 
+--- Required attributes:<br>
+---     - value=string
 return function(bodyTagHandlers, env)
     bodyTagHandlers.align = {
         start = function(xml)
+            if (xml.attributes.value ~= nil) then
+                env.logger.error("Missing value attribute in align tag")
+            end
             env.alignmentStack[#env.alignmentStack + 1] = xml.attributes.value
         end,
         final = function(xml)
